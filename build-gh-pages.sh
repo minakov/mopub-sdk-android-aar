a407931d9242ce4e0e62494a2538d5c9adcf3011
minakov <minakov@gmail.com>
2013-11-29 12:26:30 +0200

    Update build-gh-pages.sh

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+DIR=mopub-sdk-android-aar-pages
+
+# Delete any existing temporary website clone
 cd ..
-git clone -b gh-pages --single-branch https://github.com/minakov/mopub-sdk-android-aar.git mopub-sdk-android-aar-pages
+rm -rf $DIR
 
-cd mopub-sdk-android-aar
-mvn install:install-file -Dfile=build/libs/mopub-sdk-android-aar.aar -DgroupId=com.mopub.mobileads -DartifactId=mopub-android -Dversion="1.0" -Dpackaging=aar -DlocalRepositoryPath=../vf-mopub-sdk-android-aar-pages
+# Checkout and track the gh-pages branch
+git clone -b gh-pages --single-branch https://github.com/minakov/mopub-sdk-android-aar.git $DIR
 
-cd ../mopub-sdk-android-aar-pages
+# Delete everything
+cd $DIR
+rm -rf *
+
+# Copy artifacts
+cd ../mopub-sdk-android-aar
+mvn install:install-file -Dfile=build/libs/mopub-sdk-android-aar.aar -DgroupId=com.mopub.mobileads -DartifactId=mopub-android -Dversion="1.0" -Dpackaging=aar -DlocalRepositoryPath=../$DIR
+
+# Create pretty directory listing
+cd ../$DIR
 for DIR in $(find ./ \( -name mopub-android-sdk -o -name build -o -name .git -o -name .gradle \) -prune -o -type d); do
   (
     echo "<html><body><h1>Directory listing</h1><hr/><pre>"
@@ -15,3 +27,14 @@ for DIR in $(find ./ \( -name mopub-android-sdk -o -name build -o -name .git -o 
   ) > "${DIR}/index.html"
 done
 
+# Stage all files in git and create a commit
+git add .
+git add -u
+git commit -m "Website at $(date)"
+
+# Push the new files up to GitHub
+git push origin gh-pages
+
+# Delete our temp folder
+cd ..
+rm -rf $DIR
